e9d31099d532767b6ff1d9d7c93fa6e9f129f6da
zeroCalm <smith.timothyh@gmail.com>
2017-05-20 14:00:03 -0700

    timer is operating, needs formating

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Timer App built in React js</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <Timer />
+          <Timer start={Date.now()}/>
       </div>
     );
   }
--- src/timer.js
+++ src/timer.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class Timer extends Component {
+
+	componentDidMount(){
+		this.timer = setInterval(this.ticker, 1000)
+	}
+
+	ticker(){
+		this.setState({clock: new Date() - this.props.start})
+	}
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			clock: 0
+		};
+		this.ticker = this.ticker.bind(this);
+	}
 	render(){
 		return(
 			<div>
 				<p>You have been on this site since: </p> <br />
-				<span> 55.98 </span>
+				<span> {this.state.clock} </span>
 				<p> Seconds.</p>
 			</div>
 		);
